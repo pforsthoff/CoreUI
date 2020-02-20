99c57f379e3e8cb6aca97af897c4c069e684a45b
pforsthoff <pforsthoff@gmail.com>
2020-02-20 14:25:32 -0700

    added node files and dependencies

--- CoreUI/obj/Debug/netcoreapp3.1/CoreUI.AssemblyInfo.cs
+++ CoreUI/obj/Debug/netcoreapp3.1/CoreUI.AssemblyInfo.cs
@@ -11,6 +11,7 @@
 using System;
 using System.Reflection;
 
+[assembly: Microsoft.AspNetCore.Identity.UI.UIFrameworkAttribute("Bootstrap4")]
 [assembly: Microsoft.Extensions.Configuration.UserSecrets.UserSecretsIdAttribute("aspnet-CoreUI-E6635973-C359-4C69-92DB-87DD6BA1F051")]
 [assembly: System.Reflection.AssemblyCompanyAttribute("CoreUI")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
