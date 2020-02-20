38147c60009f738eb59f04f41948380260d47f33
pforsthoff <pforsthoff@gmail.com>
2020-02-20 14:51:27 -0700

    added views

---
+++ CoreUI/Controllers/CoreUIController.cs
@@ -0,0 +1,20 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CoreUI.Controllers
+{
+  [Route("CoreUI")]
+  public class CoreUIController : Controller
+  {
+    [Route("{view=Index}")]
+    public IActionResult Index(string view)
+    {
+      ViewData["Title"] = "CoreUI Free Bootstrap Admin Template";
+
+      return View(view);
+    }
+  }
+}
